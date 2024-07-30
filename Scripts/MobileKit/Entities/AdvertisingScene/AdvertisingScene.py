ed1927b53e69e94b8d7b0af875c761badc2e595f
eclipse7723 <eclipse7723@gmail.com>
2024-07-30 14:24:35 +0300

    add ad points feature

--- Scripts/MobileKit/Entities/AdvertisingScene/AdvertisingScene.py
+++ Scripts/MobileKit/Entities/AdvertisingScene/AdvertisingScene.py
@@ -1,9 +1,34 @@
 from Foundation.Entity.BaseEntity import BaseEntity
 from Foundation.TaskManager import TaskManager
+from Foundation.Providers.AdPointProvider import AdPointProvider
 from Foundation.Providers.AdvertisementProvider import AdvertisementProvider
 
 
 class AdvertisingScene(BaseEntity):
+
+    """ How To:
+
+    > from Foundation.DemonManager import DemonManager
+    > AdvertisingScene = DemonManager.getDemon("AdvertisingScene")
+
+    CASE 1: just try to show interstitial or Rewarded
+
+        > AdvertisingScene.setParams(Mode="Interstitial", AdUnitName="TestAdUnit", NextScene="Lobby")
+        > AdvertisingScene.tryStartInterstitial()
+
+    CASE 2: try to start AdPoint (trigger ad point, check if ad point is ready, do transition)
+
+        > AdvertisingScene.setParams(AdPointName="ad_interstitial_level_start", NextScene="GameArea")
+        > AdvertisingScene.tryStartInterstitial()
+
+    CASE 3: try to start few ad points in a row
+
+        > AdvertisingScene.addQueueAdPoint("ad_interstitial_level_lose")
+        > AdvertisingScene.addQueueAdPoint("ad_interstitial_level_end")
+        > # checks all ad points (trigger, check ready), show ad (if possible)
+        > AdvertisingScene.startQueueAdPoints("GameOver")
+    """
+
     def __init__(self):
         super(AdvertisingScene, self).__init__()
         self.tc = None
@@ -15,6 +40,12 @@ class AdvertisingScene(BaseEntity):
         Type.addAction(Type, "CacheNoAds")
         Type.addAction(Type, "Mode")
         Type.addAction(Type, "AdUnitName")
+        Type.addAction(Type, "AdPointName")
+
+    def _onPreparation(self):
+        if _DEVELOPMENT is True and self.object.hasObject("Movie2_Content"):
+            movie = self.object.getObject("Movie2_Content")
+            movie.setEnable(True)
 
     def _onActivate(self):
         self.__runTaskChain()
@@ -25,22 +56,36 @@ class AdvertisingScene(BaseEntity):
             self.tc = None
 
     def __runTaskChain(self):
+        if self.tc is not None:
+            self.tc.cancel()
         self.tc = TaskManager.createTaskChain()
 
         with self.tc as tc:
             tc.addScope(self._scopeMain)
             tc.addDelay(1)  # necessary for transition
-            tc.addFunction(self.object.runNextScene)
+            with tc.addIfTask(lambda: len(self.object.ad_points_queue) == 0) as (if_empty, if_not_empty):
+                if_empty.addNotify(Notificator.onChangeScene, self.NextScene)
+                if_not_empty.addFunction(self.__runTaskChain)
 
     def _scopeMain(self, source):
-        source.addPrint("[AdvertisingScene] Mode [{}] AdUnitName [{}] prepared".format(self.Mode, self.AdUnitName))
+        if len(self.object.ad_points_queue) != 0:
+            ad_point_params = AdPointProvider.getAdPointParams(self.object.ad_points_queue.pop(0))
+            self.object.setParam("Mode", ad_point_params.ad_type)
+            self.object.setParam("AdUnitName", ad_point_params.ad_unit_name)
+            self.object.setParam("AdPointName", ad_point_params.name)
 
-        if self.Mode == "Interstitial":
+        if self.AdPointName is not None:
+            source.addScope(self._scopeAdPoint)
+        elif self.Mode == "Interstitial":
             source.addScope(self._scopeInterstitial)
         elif self.Mode == "Rewarded":
             source.addScope(self._scopeRewarded)
         else:
             Trace.log("Entity", 0, "AdvertisingScene: Unknown Mode: {}".format(self.Mode))
+
+        source.addFunction(self.object.setParam, "AdUnitName", None)
+        source.addFunction(self.object.setParam, "AdPointName", None)
+        source.addFunction(self.object.setParam, "Mode", None)
 
     def _scopeRewarded(self, source):
         with source.addParallelTask(2) as (response, request):
@@ -56,3 +101,14 @@ class AdvertisingScene(BaseEntity):
                 response_ok.addListener(Notificator.onAdvertHidden)
                 response_fail.addListener(Notificator.onAdvertDisplayFailed)
             request.addFunction(AdvertisementProvider.showAdvert, "Interstitial", self.AdUnitName)
+
+    def _scopeAdPoint(self, source):
+        with source.addParallelTask(2) as (response, request):
+            # react on advert
+            with response.addRaceTask(2) as (hidden, fail):
+                hidden.addListener(Notificator.onAdvertHidden)
+                fail.addListener(Notificator.onAdvertDisplayFailed)
+
+            # reset counter and show advert
+            request.addFunction(AdPointProvider.startAdPoint, self.AdPointName)
+
--- Scripts/MobileKit/Objects/ObjectAdvertisingScene.py
+++ Scripts/MobileKit/Objects/ObjectAdvertisingScene.py
@@ -1,9 +1,11 @@
 from Foundation.Object.DemonObject import DemonObject
 from Foundation.SystemManager import SystemManager
-from Foundation.Providers.AdvertisementProvider import AdvertisementProvider
+from Foundation.Providers.AdPointProvider import AdPointProvider
 
 
 class ObjectAdvertisingScene(DemonObject):
+    ad_points_queue = []
+    base_scene_name = "Advertising"
 
     @staticmethod
     def declareORM(Type):
@@ -12,6 +14,7 @@ class ObjectAdvertisingScene(DemonObject):
         Type.addParam(Type, "CacheNoAds")
         Type.addParam(Type, "Mode")
         Type.addParam(Type, "AdUnitName")
+        Type.addParam(Type, "AdPointName")
 
     def _onParams(self, params):
         super(ObjectAdvertisingScene, self)._onParams(params)
@@ -19,14 +22,17 @@ class ObjectAdvertisingScene(DemonObject):
         self.initParam("CacheNoAds", params, False)
         self.initParam("Mode", params, "Interstitial")
         self.initParam("AdUnitName", params, None)
+        self.initParam("AdPointName", params, None)
 
-    def isInterstitialReady(self):
-        if Mengine.hasOption("noads") is True:
-            return False
-
+    def isInterstitialEnabled(self):
         if self.getParam("CacheNoAds") is True:
             return False
-        if Mengine.getConfigBool('Advertising', "Interstitial", False) is False:
+
+        if Mengine.getConfigBool("Advertising", "Interstitial", False) is False:
+            return False
+        if Mengine.hasTouchpad() is False:
+            if _DEVELOPMENT is True:
+                Trace.msg_err("Advertising works only with touchpad! (add -touchpad)")
             return False
 
         SystemAdvertising = SystemManager.getSystem("SystemAdvertising")
@@ -35,23 +41,75 @@ class ObjectAdvertisingScene(DemonObject):
             self.setParam("CacheNoAds", True)
             return False
 
-        SystemAdvertising.increaseTriggerCounter()
+        return True
 
-        if SystemAdvertising.isReadyToView() is False:
+    def skipInterstitial(self):
+        Notification.notify(Notificator.onChangeScene, self.getParam("NextScene"))
+
+    def tryStartInterstitial(self):
+        """ returns True if ad will be shown
+            do not forget to put all object params before call this method!
+        """
+        if self._tryStartInterstitial() is False:
+            self.skipInterstitial()
             return False
 
-        if SystemAdvertising.isInterstitialParamEnable("trigger") is True:
-            # time-based ads already check availability in isReadyToView #fixme
-            ad_unit_name = self.getParam("AdUnitName")
-            if AdvertisementProvider.isAdvertAvailable("Interstitial", ad_unit_name) is False:
-                return False
+        Notification.notify(Notificator.onChangeScene, self.base_scene_name)
+        return True
+
+    def _checkAdPoint(self, ad_point_name):
+        """ checks if interstitial is allowed and ad point exists, enabled """
+        if self.isInterstitialEnabled() is False:
+            return False
+
+        if ad_point_name is None:
+            Trace.log("Object", 0, "AdPointName not found")
+            return False
+
+        if AdPointProvider.hasAdPoint(ad_point_name) is False:
+            Trace.log("Object", 0, "AdPoint {!r} not found".format(ad_point_name))
+            return False
+
+        if AdPointProvider.isEnabledAdPoint(ad_point_name) is False:
+            return False
 
         return True
 
-    def runNextScene(self):
-        next_scene = self.getParam("NextScene")
-        Notification.notify(Notificator.onChangeScene, next_scene)
+    def _tryStartInterstitial(self):
+        """ trigger and check ad point if ready to show ad """
+        ad_point_name = self.getParam("AdPointName")
 
-    def runAdvertScene(self):
-        Notification.notify(Notificator.onChangeScene, "Advertising")
+        if self._checkAdPoint(ad_point_name) is False:
+            return False
+
+        ad_point_params = AdPointProvider.getAdPointParams(ad_point_name)
+        AdPointProvider.triggerAdPoint(ad_point_name)
+        if AdPointProvider.checkAdPoint(ad_point_name) is False:
+            return False
+
+        self.setParams(AdUnitName=ad_point_params.ad_unit_name, Mode=ad_point_params.ad_type)
+        return True
+
+    def addQueueAdPoint(self, ad_point_name):
+        if self._checkAdPoint(ad_point_name) is False:
+            return False
+
+        self.ad_points_queue.append(ad_point_name)
+        return True
+
+    def startQueueAdPoints(self, next_scene):
+        self.setParam("NextScene", next_scene)
+
+        ready_ad_points = []
+        for ad_point_name in self.ad_points_queue:
+            AdPointProvider.triggerAdPoint(ad_point_name)
+            if AdPointProvider.checkAdPoint(ad_point_name) is True:
+                ready_ad_points.append(ad_point_name)
+
+        self.ad_points_queue = ready_ad_points
+
+        if len(self.ad_points_queue) > 0:
+            Notification.notify(Notificator.onChangeScene, self.base_scene_name)
+        else:
+            self.skipInterstitial()
 
